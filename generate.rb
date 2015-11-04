5db72adb0d315900961afbeb55bdcde7041725d6
Danny Burrows <burrows.danny@gmail.com>
2015-11-04 15:28:35 -0600

    Added optionsparse, yaml libs

--- generate.rb
+++ generate.rb
@@ -1,1 +1,14 @@
 #!/usr/bin/env Ruby
+
+require 'yaml'
+require 'optparse'
+
+def main
+end
+
+options = {}
+OptionsParser.new do |opt|
+    opts.banner = "Usage: generate.rb <file_type (VB, C#)> <yaml_file> [options]"
+end.parse!
+
+main()
